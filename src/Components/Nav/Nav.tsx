ba551e4f14daf9081005357178647824a6c1c0c1
Manjil Ghale <rilleymanjil@hotmail.com>
2022-12-31 16:54:18 +0000

    Nav completed, needs smooth scroll animation

--- src/Components/Nav/Nav.tsx
+++ src/Components/Nav/Nav.tsx
@@ -1,4 +1,5 @@
 import '../Style/style.css'
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 
 function Nav() {
   return (
@@ -7,6 +8,10 @@ function Nav() {
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
             <li><a href="#projects">Projects</a></li>
+            <ul>
+              <li><a href=""><AiFillGithub size={24} /></a></li>
+              <li><a href=""><AiFillLinkedin size={24} /></a></li>
+            </ul>
         </ul>
     </div>
   )
